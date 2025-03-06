2d3391fed449a31512f6c361f53c956cfa7dd17a
vstlvl <zorix228337@gmail.com>
2025-03-06 18:44:02 +0300

    добавил функцию суммы левого нижнего треугольника матрицы

--- arr_funcs.py
+++ arr_funcs.py
@@ -26,10 +26,25 @@ def filter_not_equal(arr, value):
 
     return new_arr
 
+def left_triangle_sum(matrix):
+    sum = 0
+    for i in range(0, len(matrix)):
+        for k in range(0, i + 1):
+            sum += matrix[i][k]
+
+    return sum
+
 def test():
     test_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    test_matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
 
     print(filter_greater(test_arr, 5))
     print(filter_less(test_arr, 5))
     print(filter_equal(test_arr, 5))
     print(filter_not_equal(test_arr, 5))
+
+    print(left_triangle_sum(test_matrix))
