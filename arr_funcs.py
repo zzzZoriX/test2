221b73aaef88288d7a035142fb347ca3eba82369
vstlvl <zorix228337@gmail.com>
2025-03-06 18:33:13 +0300

    добавил фукцию теста

--- arr_funcs.py
+++ arr_funcs.py
@@ -18,10 +18,18 @@ def filter_equal(arr, value):
         if(el == value): new_arr.append(el)
 
     return new_arr
-    
+
 def filter_not_equal(arr, value):
     new_arr = []
     for el in arr:
         if(el != value): new_arr.append(el)
 
     return new_arr
+
+def test():
+    test_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+    print(filter_greater(test_arr, 5))
+    print(filter_less(test_arr, 5))
+    print(filter_equal(test_arr, 5))
+    print(filter_not_equal(test_arr, 5))
